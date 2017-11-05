3da8e16f9a132babae7a130a0414277dcb858c27
Ench0 <ensar@farend.net>
2017-11-05 17:29:09 +0000

    upd

--- routes/index.js
+++ routes/index.js
@@ -13,34 +13,97 @@ const update = require('prayer-timetable').update;
 const present = require('../controllers/present');
 
 
+
 const auth = require('http-auth');
-var basic = auth.basic({
+
+const fs = require('fs-extra')
+const confdir = './config'
+const passfile = './config/user.pass'
+
+const cityfile = './public/db/city.js'
+const settingsfile = './public/db/settings.js'
+
+
+// check browser city file
+if (fs.existsSync(cityfile) && fs.statSync(cityfile).size>0) {
+    console.log("#####city file ok#####")
+    console.log("city file size: "+fs.statSync(cityfile).size)
+}
+else {
+    console.log("#####pass write#####")
+    fs.writeFileSync(cityfile, timetabledef)
+}
+
+// check browser settings file
+if (fs.existsSync(settingsfile) && fs.statSync(settingsfile).size>0) {
+    console.log("#####settings file ok#####")
+    console.log("settings file size: "+fs.statSync(settingsfile).size)
+}
+else {
+    console.log("#####pass write#####")
+    fs.writeFileSync(settingsfile, settingsdef)
+}
+
+
+
+// check user pass file
+if (fs.existsSync(passfile) && fs.statSync(passfile).size>0) {
+  console.log("#####pass file ok#####")
+  console.log("pass file size: "+fs.statSync(passfile).size)
+}
+else {
+  console.log("#####pass write#####")
+  fs.ensureDirSync(confdir)    
+  fs.writeFileSync(passfile, 'admin:$2a$10$XNuKAWZ6Qx/pXnYGkRT67OKJ8RlVTHdOZOw6nIC0CNg5Y1JbJy2r6')
+}
+
+
+
+const basic = auth.basic({
     realm: "Admin Area",
-    file: __dirname + "/../config/user.pass"
+    // file: __dirname + "/user.pass"
+    file: "./config/user.pass"
 });
+
+
 
 
 router.get('/present', present.view);
 
-// router.get('/', function(req, res, next) {
-//     res.render('present', { title: 'Present', settings: settings, timetabledef: timetabledef, settingsdef: settingsdef });
-//   });
 
 router.get('/timetable', function(req, res, next) {
-    res.render('timetable', { title: 'Timetable', settings: settings, timetabledef: timetabledef, settingsdef: settingsdef });
-  });
+    console.log("settings")
+  res.render('timetable', { title: 'Timetable', settings: settings, timetabledef: timetabledef, settingsdef: settingsdef });
+});
 
-  
-  
-  /* GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id) */
-  router.get('/admin', auth.connect(basic), admin.admin_get);
-  
-  /* POST request for creating Genre. */
-  router.post('/admin', auth.connect(basic), admin.admin_post);
-  
 
+
+// MOBILE ROUTES
+router.get('/', function(req, res, next) {
+
+  if (req.cookies.mobset) var mobset = req.cookies.mobset
+  else mobset = ['', '', '', '#ccc', '/img/paper.png', '']
+  res.render('mobile', { title: 'Timetable', settings: settings, mobset:mobset, timetabledef: timetabledef, settingsdef: settingsdef, body: req.body });
   
-  // Password form
+});
+
+router.post('/', function(req, res, next) {
+ 
+  res.cookie('mobset', [req.body.analogue || '', req.body.jamaah || '', req.body.arabic || '', req.body.themecol || '#ccc', req.body.themeimg || '/img/paper.png', 'cookies agree'], { maxAge: 31556952000, httpOnly: false })//.send('Cookie is set');
+  res.redirect('/mobile')
+
+});
+
+
+
+
+/* GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id) */
+router.get('/admin', auth.connect(basic), admin.admin_get);
+
+/* POST request for creating Genre. */
+router.post('/admin', auth.connect(basic), admin.admin_post);
+
+// Password form
 router.get('/admin/password', auth.connect(basic), admin.pass_get);
 
 // password post
@@ -53,22 +116,4 @@ router.get('/reboot', auth.connect(basic), update.reboot);
 router.get('/view', admin.view);
 
 
-
-// MOBILE ROUTES
-router.get('/', function(req, res, next) {
-    
-      if (req.cookies.mobset) var mobset = req.cookies.mobset
-      else mobset = ['', '', '', '#ccc', '/img/paper.png', '']
-      res.render('mobile', { title: 'Timetable', settings: settings, mobset:mobset, timetabledef: timetabledef, settingsdef: settingsdef, body: req.body });
-      
-});
-    
-router.post('/', function(req, res, next) {
-    
-    res.cookie('mobset', [req.body.analogue || '', req.body.jamaah || '', req.body.arabic || '', req.body.themecol || '#ccc', req.body.themeimg || '/img/paper.png', 'cookies agree'], { maxAge: 31556952000, httpOnly: false })//.send('Cookie is set');
-    res.redirect('/')
-
-});
-
-  
 module.exports = router;
